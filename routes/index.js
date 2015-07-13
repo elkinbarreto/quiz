404dd282422ccf78f92d52c0ae00788fd643fb55
elkinbarreto <nikle2828@gmail.com>
2015-07-13 15:13:41 -0500

    Revert "Quiz versión 7: Ejercicio obligatorio p2p Modulo  6"
    This reverts commit c35c1b13da0be0522a0ab05b96de2996bc4436ac.

--- routes/index.js
+++ routes/index.js
@@ -11,7 +11,4 @@ router.get('/', function(req, res) {
 router.get('/quizes/question', quizController.question);
 router.get('/quizes/answer', quizController.answer);
 
-router.get('/author',function(req, res){
-	res.render('author',{title:'Autor'})
-});
 module.exports = router;
