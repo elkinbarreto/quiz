d4620f9227544793b62474631586349be2efdd04
elkinbarreto <nikle2828@gmail.com>
2015-07-29 09:15:47 -0500

    Version Despliegue BD en Heroku

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -1,15 +1,23 @@
+var models = require('../models/models.js');
+
 //GET /quizes/question
 exports.question = function(req, res){
-	res.render('quizes/question',{pregunta:'Capital de Italia'});
+	//res.render('quizes/question',{pregunta:'Capital de Italia'});
+	models.Quiz.findAll().success(function(quiz){
+		res.render('quizes/question',{pregunta:quiz[0].pregunta});
+	}); 
 
 };
 
 //GET /quizes/answer
 exports.answer = function(req, res){
-	if(req.query.repuesta === 'Roma'){
-		res.render('quizes/answer',{repuesta:'Correcto!'});
-	}else{
-		res.render('quizes/answer',{repuesta:'Incorrecto!'});
-	}
+	models.Quiz.findAll().success(function(quiz){
+		if(req.query.repuesta === quiz[0].repuesta){
+			res.render('quizes/answer',{repuesta:'Correcto!'});
+		}else{
+			res.render('quizes/answer',{repuesta:'Incorrecto!'});
+		}
+	});
+	
 
 };
