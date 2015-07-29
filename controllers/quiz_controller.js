2ae79c3ee53df8ea494de97c43c04b40205ccb6d
elkinbarreto <nikle2828@gmail.com>
2015-07-29 10:43:08 -0500

    Version 8: Despliegue DB en Heroku

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 var models = require('../models/models.js');
 
 //GET /quizes/question
@@ -7,17 +7,10 @@ exports.question = function(req, res){
 	models.Quiz.findAll().success(function(quiz){
 		res.render('quizes/question',{pregunta:quiz[0].pregunta});
 	}); 
-=======
-//GET /quizes/question
-exports.question = function(req, res){
-	res.render('quizes/question',{pregunta:'Capital de Italia'});
->>>>>>> 404dd282422ccf78f92d52c0ae00788fd643fb55
-
-};
 
 //GET /quizes/answer
 exports.answer = function(req, res){
-<<<<<<< HEAD
+
 	models.Quiz.findAll().success(function(quiz){
 		if(req.query.repuesta === quiz[0].repuesta){
 			res.render('quizes/answer',{repuesta:'Correcto!'});
@@ -26,12 +19,4 @@ exports.answer = function(req, res){
 		}
 	});
 	
-=======
-	if(req.query.repuesta === 'Roma'){
-		res.render('quizes/answer',{repuesta:'Correcto!'});
-	}else{
-		res.render('quizes/answer',{repuesta:'Incorrecto!'});
-	}
->>>>>>> 404dd282422ccf78f92d52c0ae00788fd643fb55
-
 };
