768328be30d7b8af0bec1345256c270ed7260b6a
elkinbarreto <nikle2828@gmail.com>
2015-08-07 20:07:50 -0500

    Version 19: Ejercicio obligatoriomod7 - Buscar preguntas

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -16,9 +16,29 @@ exports.load = function(req, res, next, quizId){
 
 //GET /quizes
 exports.index = function(req,res,next){
-	models.Quiz.findAll().then(function(quizes){
+	if(req.query.search){
+
+		var buscar = req.query.search.replace(" ","%");
+
+		models.Quiz.findAll(
+		{
+			where:["pregunta like ?","%"+buscar+"%"]
+
+		}).then(function(quizes){
+
+				res.render('quizes/index.ejs',{quizes:quizes,errors:[]});
+
+		}).catch(function(error){next(error)});
+	}
+	else{
+
+		models.Quiz.findAll().then(function(quizes){
+
 		res.render('quizes/index.ejs',{quizes:quizes,errors:[]});
-	}).catch(function(error){next(error)});
+
+		}).catch(function(error){next(error)});
+	}
+	
 };
 
 //GET /quizes/:id
